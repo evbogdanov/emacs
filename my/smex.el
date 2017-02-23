8e7d402368cda9289ae0636a86fe724a7bc2e6b4
Ev Bogdanov <evbogdanov.com@gmail.com>
2017-02-23 13:34:12 +0300

    remap help key

--- my/smex.el
+++ my/smex.el
@@ -8,5 +8,5 @@
 (add-hook 'ido-setup-hook
           (lambda ()
             (define-key ido-completion-map (kbd "C-h")    'delete-backward-char)
-            (define-key ido-completion-map (kbd "<f1> f") 'smex-describe-function)
-            (define-key ido-completion-map (kbd "<f1> w") 'smex-where-is)))
+            (define-key ido-completion-map (kbd "§ f") 'smex-describe-function)
+            (define-key ido-completion-map (kbd "§ w") 'smex-where-is)))
